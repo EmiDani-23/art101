ce97035d037d3490fd4f73768b118908d1e6daa0
EmiDani-23 <EmilyBarete230@gmail.com>
2024-10-28 17:04:37 -0700

    lab 5 input fix

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -14,11 +14,11 @@ year = 2012;
 age = 2024 - year; 
 
 //output
-Document.writeln("make: " + make + "<br>");
-Document.writeln("model: " + model + "<br>");
-Document.writeln("color: " + color + "<br>");
-Document.writeln("year: " + year + "<br>");
-Document.writeln("age: " + age + "<br>");
+document.writeln("make: " + make + "<br>");
+document.writeln("model: " + model + "<br>");
+document.writeln("color: " + color + "<br>");
+document.writeln("year: " + year + "<br>");
+document.writeln("age: " + age + "<br>");
 
 // Functions
 
